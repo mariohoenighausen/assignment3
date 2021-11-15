5024e913a20f7b5be4773637a8fdbf9de9c7a5c7
mariohoenighausen <mario.hoenighausen@smail.inf.h-brs.de>
2021-11-15 14:25:59 +0100

    Added employee get request

--- routes/orangeHrm.js
+++ routes/orangeHrm.js
@@ -4,21 +4,32 @@ const router = express.Router();
 const axios = require('axios');
 const qs = require('querystring');
 
+const baseUrl = 'https://sepp-hrm.inf.h-brs.de/symfony/web/index.php/';
+    
+const body = qs.stringify({ client_id: 'api_oauth_id', client_secret: 'oauth_secret', grant_type: 'password', username: 'demouser', password: '*Safb02da42Demo$'});
+const config = { headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'application/json',
+    }
+    };
+// const getConfig = { headers: {
+//         'Accept': 'application/json',
+//         'Authorization' : `Bearer ${token}`,
+//         }
+//         };
+    
+    
+
+var token;
 //orangehrm
 router.get('/orangeHrm', async function(req,res,next){
-    const baseUrl = 'https://sepp-hrm.inf.h-brs.de/symfony/web/index.php/';
-    
-    const body = qs.stringify({ client_id: 'api_oauth_id', client_secret: 'oauth_secret', grant_type: 'password', username: 'demouser', password: '*Safb02da42Demo$'});
-    const config = { headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json', }
-        };
+   
         try{
-            const result = await axios.post(`${baseUrl}oauth/issueToken`, body, config); 
-            if (result.data.error) {
-                throw Error(result.data.error); 
+            token = await axios.post(`${baseUrl}oauth/issueToken`, body, config); 
+            if (token.data.error) {
+                throw Error(token.data.error); 
             }
-            let accessToken = result.data['access_token']; 
+            let accessToken = token.data['access_token']; 
             console.log(`Here is the access Token: ${accessToken}`);
             res.send({msg: "Here are all the posts!",accessToken})
         }
@@ -27,7 +38,27 @@ router.get('/orangeHrm', async function(req,res,next){
         }
 });
 
-router.get('/orangeHrm/test', async function(req,res,next){
+router.get('/orangeHrm/employees', async function(req,res,next){
+    //https://sepp-hrm.inf.h-brs.de/symfony/web/index.php/api/v1/employee/1
+    try{
+        token = await axios.post(`${baseUrl}oauth/issueToken`, body, config); 
+        getConfig = { headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json',
+            'Authorization' : `Bearer ${token.data["access_token"]}`,
+            }
+            };
     
+        console.log(token.data["access_token"]);
+        const result = await axios.get('api/v1/employee/1', {}, getConfig)
+        
+        if (result.data.error) {
+            throw Error(result.data.error); 
+        }
+        res.send({msg: "Here are all the posts!",result})
+    }
+    catch(error){
+        next(error);
+    }
 })
 module.exports = router;
